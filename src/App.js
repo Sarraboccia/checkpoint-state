b2370ba184a6b9a3629d5d12da7801a73d2c0120
Sara Boccia <saraboccia@Saras-MacBook-Air.local>
2021-11-04 14:38:38 +0100

    checkpoint state first version

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import React, { Component } from "react";
+import Person from "./Person/Person";
+import Button from "react-bootstrap/Button";
+
+class App extends Component {
+  state = {
+    isToggleOn: false,
+  
+  };
+
+  //setInterval(() => {
+   // this.setState({ delay: this.state.delay + 1 });
+ // }, 1000);
+
+
+
+  handleClick() {
+    //change the state
+    this.setState({
+      isToggleOn: !this.state.isToggleOn,
+    });
+  }
+  
+
+  render() {
+    return (
+      <div className="App">
+        <Button onClick={() => this.handleClick()}>
+          {this.state.isToggleOn ? "ON" : "OFF" }
+        </Button>
+        <br />
+
+        {this.state.isToggleOn ? <Person /> : null}
+      </div>
+    );
+  }
+}
+
+export default App;
